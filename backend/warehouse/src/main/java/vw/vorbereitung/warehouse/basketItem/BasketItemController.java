11c40c12894cea721dfbd8d3d10f95c73fb82ddd
Kowall, Marian <marian.kowall@volkswagen.de>
2022-12-01 08:18:24 +0100

    basic logging work

--- backend/warehouse/src/main/java/vw/vorbereitung/warehouse/basketItem/BasketItemController.java
+++ backend/warehouse/src/main/java/vw/vorbereitung/warehouse/basketItem/BasketItemController.java
@@ -1,6 +1,8 @@
 package vw.vorbereitung.warehouse.basketItem;
 
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,14 @@ import java.util.UUID;
 public class BasketItemController {
 
   private final BasketItemService service;
+
+  private final Logger logger = LoggerFactory.getLogger(BasketItemController.class);
+
+  @RequestMapping("/")
+  public String home() {
+    this.logger.info("Logger works");
+    return "HOME";
+  }
 
   @GetMapping("/item")
   public ResponseEntity<List<BasketItemDocument>> getAll() {
