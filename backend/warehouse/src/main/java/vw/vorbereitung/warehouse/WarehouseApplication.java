21dd50898af3169681e39de7f3f8227e76c6b349
Kowall, Marian <marian.kowall@volkswagen.de>
2022-12-15 16:54:17 +0100

    add swagger for some reason

--- backend/warehouse/src/main/java/vw/vorbereitung/warehouse/WarehouseApplication.java
+++ backend/warehouse/src/main/java/vw/vorbereitung/warehouse/WarehouseApplication.java
@@ -4,32 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
-import vw.vorbereitung.warehouse.basketItem.BasketItemRepository;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import vw.vorbereitung.warehouse.basketItem.BasketItemService;
 import vw.vorbereitung.warehouse.basketItem.model.BasketItemDocument;
 
 import java.util.UUID;
 
 @SpringBootApplication
+@EnableSwagger2
 public class WarehouseApplication {
 
-	@Autowired
-	BasketItemService service;
+  @Autowired BasketItemService service;
 
-	public static void main(String[] args) {
-		SpringApplication.run(WarehouseApplication.class, args);
-	}
-	@EventListener(ApplicationReadyEvent.class)
-	public void doSomethingAfterStartup() {
-		System.out.println("hello world, I have just started up");
-		BasketItemDocument test = BasketItemDocument.builder()
-													.itemNumber((UUID.randomUUID()))
-													.productName("Hello")
-													.purchasingPrice(2)
-													.sellingPrice(1).manufacturer("Manfred").build();
+  public static void main(String[] args) {
+    SpringApplication.run(WarehouseApplication.class, args);
+  }
 
-		service.save(test);
-	}
+  @EventListener(ApplicationReadyEvent.class)
+  public void doSomethingAfterStartup() {
+    System.out.println("hello world, I have just started up");
+    BasketItemDocument test =
+        BasketItemDocument.builder()
+            .itemNumber((UUID.randomUUID()))
+            .productName("Hello")
+            .purchasingPrice(2)
+            .sellingPrice(1)
+            .manufacturer("Manfred")
+            .build();
+
+    this.service.save(test);
+  }
 }
