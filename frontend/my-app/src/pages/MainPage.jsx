49eee41bdf7894478966992b4fb1d1fca903de15
Kowall, Marian <marian.kowall@volkswagen.de>
2023-01-02 18:11:50 +0100

    works with router

--- frontend/my-app/src/pages/MainPage.jsx
+++ frontend/my-app/src/pages/MainPage.jsx
@@ -1,29 +1,28 @@
-import {ThemeProvider} from "@mui/material";
-import {Theme} from "../Theme/Theme"
+import { ThemeProvider } from "@mui/material";
+import { Theme } from "../Theme/Theme";
 import MenuAppBarComponent from "../components/appbar/MenuAppBarComponent";
-import {LINK_BASKETITEM_PAGE,LINK_STARTPAGE } from "../globalvariables/LinkUrl"
-import StartPage from "./StartPage"
-import BasketItemPage from './BasketItemPage'
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-
+import {
+  LINK_BASKETITEM_PAGE,
+  LINK_STARTPAGE,
+} from "../globalvariables/LinkUrl";
+import StartPage from "./StartPage";
+import BasketItemPage from "./BasketItemPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const MainPage = () => {
   return (
     <>
-    <ThemeProvider theme={Theme}>
-    <MenuAppBarComponent/>
-
-    <Router>
-    
-      <Routes>
-      <Route path={LINK_STARTPAGE} element={<StartPage/>}/>
-      <Route path={LINK_BASKETITEM_PAGE} element={<BasketItemPage/>}/>
-      </Routes>
-      </Router>
-    
+      <ThemeProvider theme={Theme}>
+        <MenuAppBarComponent />
+        <Router>
+          <Routes>
+            <Route path={LINK_STARTPAGE} element={<StartPage />} />
+            <Route path={LINK_BASKETITEM_PAGE} element={<BasketItemPage />} />
+          </Routes>
+        </Router>
       </ThemeProvider>
     </>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
